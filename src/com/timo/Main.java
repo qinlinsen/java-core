d410fe6321e7f65d72d811ef1edd40d5c9b1934a
qinlinsen <qinlinsen@cloudwalk.cn>
2018-07-20 11:35:19 +0800

    test String.join useage ！

---
+++ src/com/timo/Main.java
@@ -0,0 +1,16 @@
+package com.timo;
+
+import java.util.Objects;
+
+/**
+ * @author qinlinsen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Objects.requireNonNull(null);
+        String join = String.join("", "m", "y", "n", "e");
+        System.out.println("join="+join);
+    }
+}
